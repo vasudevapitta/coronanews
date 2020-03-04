20cac7760b5b3b6570838b6f87e6fe1f92dde697
vasudevapitta <tizzicboy@gmail.com>
2020-03-04 00:11:45 -0600

    Initial commit

---
+++ js/app es6.js
@@ -0,0 +1,49 @@
+$(()=>{
+
+const API_KEY = '4b64ca4f39cb4f52a5ff2843d6f01541';
+const div = $('#doc');
+const country='us';
+const url=`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${API_KEY}`;
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+        $(data).each(function(ind, arrVal){
+          createTemplate(arrVal.articles);
+        });
+});
+  
+  function createTemplate(data) {
+    $(data).each(function(ind, arrVal) {
+      const title = arrVal.title;
+      const titleSearch = title.search('corona');
+      const content = arrVal.content?arrVal.content:'null';
+      const contentSearch = content.search('corona');
+      const desc = arrVal.description;
+      const descSearch = desc.search('corona');
+      const isCorona = (titleSearch!=-1 || contentSearch!=-1 || descSearch!=-1)
+      
+      if(isCorona){
+        console.log('corona news found');
+        const date = arrVal.publishedAt?arrVal.publishedAt.slice(0,10):'';
+        const author = arrVal.author?arrVal.author:'Unknown';
+
+        const readMoreUrl = arrVal.url;
+        const img = arrVal.urlToImage;
+        const source = arrVal.source.name;
+              
+        const template = `<h2>${title}</h2>
+        <p>${date}</p>
+        <figure>
+        <img src="${img}" alt="${title}" class="img">
+        <figcaption>${desc}</figcaption>
+        </figure>
+        <p>${content} <span><a href="${readMoreUrl}">Read more</a></span></p>
+        <h6>Author ${author}</h6>
+        <hr>`;
+       $(div).append(template);
+      }
+    });
+  }
+   
+});
