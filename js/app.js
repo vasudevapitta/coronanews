9a70bf30939d74f17e9b6d5086ca060858edfa77
Vasudeva Pitta <38531141+vasudevapitta@users.noreply.github.com>
2020-03-04 16:06:30 -0600

    search keyword corona in content, headline

--- js/app.js
+++ js/app.js
@@ -22,6 +22,12 @@ $.ajax({
       var content = arrVal.content ? arrVal.content : 'null';
       var desc = arrVal.description;
       
+      var titleSearch = title.search('corona');
+      var contentSearch = content.search('corona');
+      var descSearch = desc.search('corona');	
+      var isCorona = titleSearch != -1 || contentSearch != -1 || descSearch != -1;
+      
+      if (isCorona) {
         var date = arrVal.publishedAt ? arrVal.publishedAt.slice(0, 10) : '';
         var author = arrVal.author ? arrVal.author : 'Unknown';
         var readMoreUrl = arrVal.url;
@@ -29,6 +35,7 @@ $.ajax({
         var source = arrVal.source.name;
         var template = "<h2>".concat(title, "</h2>\n <p>").concat(date, "</p>\n <figure>\n <img src=\"").concat(img, "\" alt=\"").concat(title, "\" class=\"img\">\n <figcaption>").concat(desc, "</figcaption>\n </figure>\n  <p>").concat(content, " <span><a href=\"").concat(readMoreUrl, "\" target=\"_blank\" rel=\"noopener\">Read more</a></span></p>\n  <h6>Author ").concat(author, "</h6>\n <hr>");
         $(div).append(template);
+      }
     });
   }
 });
