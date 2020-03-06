f3850d935653a17b6e119372f081290c74749ed4
Vasudeva Pitta <38531141+vasudevapitta@users.noreply.github.com>
2020-03-06 10:17:08 -0600

    refactor code, remove null issues

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@ $(function () {
   var div = $('#doc');
   var country = 'us';
   var url = "https://newsapi.org/v2/everything?q=corona&sortBy=publishedAt&language=en&apiKey=".concat(API_KEY);
-
+  var templateStr = '';
+    
 $.ajax({
   dataType: "json",
   url: url,
@@ -16,26 +17,35 @@ $.ajax({
   });
 });
 
-  function createTemplate(data) {
-    $(data).each(function (ind, arrVal) {
-      var title = arrVal.title?arrVal.title:'';
-      var content = arrVal.content?arrVal.content:'';
-      var desc = arrVal.description?arrVal.description:'';
-      
-      var titleSearch = title?title.search('corona'):-1;
-      var contentSearch = content?content.search('corona'):-1;
-      var descSearch = desc?desc.search('corona'):-1;	
-      var isCorona = titleSearch != -1 || contentSearch != -1 || descSearch != -1;
-      
-      if (isCorona) {
-        var date = arrVal.publishedAt ? arrVal.publishedAt.slice(0, 10) : '';
-        var author = arrVal.author ? arrVal.author : 'Unknown';
-        var readMoreUrl = arrVal.url;
-        var img = arrVal.urlToImage;
-        var source = arrVal.source.name;
-        var template = "<h2>".concat(title, "</h2>\n <p>").concat(date, "</p>\n <figure>\n <img src=\"").concat(img, "\" alt=\"").concat(title, "\" class=\"img\">\n <figcaption>").concat(desc, "</figcaption>\n </figure>\n  <p>").concat(content, " <span><a href=\"").concat(readMoreUrl, "\" target=\"_blank\" rel=\"noopener\">Read more</a></span></p>\n  <h6>Author -").concat(author, "</h6>\n <hr>");
-        $(div).append(template);
-      }
-    });
-  }
+function createTitle(arrVal) {
+  templateStr += arrVal.title ? "<h2>".concat(arrVal.title, "</h2>") : '';
+}
+
+function createDate(arrVal) {
+  templateStr += arrVal.publishedAt ? "<p>".concat(arrVal.publishedAt.slice(0,10), "</p>") : '';
+}
+
+function createFig(arrVal) {
+  templateStr += arrVal.urlToImage ? "<figure>\n      <img src=\"".concat(arrVal.urlToImage, "\" alt=\"").concat(arrVal.title, "\" class=\"img\">\n      <figcaption>").concat(arrVal.description, "</figcaption>\n      </figure>") : '';
+}
+
+function createContent(arrVal) {
+  templateStr += arrVal.content ? "<p>".concat(arrVal.content, " <span><a href=\"").concat(arrVal.url, "\" target=\"_blank\" rel=\"noopener\">Read more</a></span></p>") : '';
+}
+
+function createAuthor(arrVal) {
+  templateStr += arrVal.author ? "<h6>Author- ".concat(arrVal.author, "</h6>") : '';
+}
+
+function createTemplate(data) {
+  $(data).each(function (ind, arrVal) {
+    createTitle(arrVal);
+    createDate(arrVal);
+    createFig(arrVal);
+    createContent(arrVal);
+    createAuthor(arrVal);
+  });
+    $(div).html(templateStr);
+}
+    
 });
